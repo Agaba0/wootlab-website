2cf8e44e7377734a8f0fa9a0abddf8a8e7090498
Agaba0 <agabasylvester0@gmail.com>
2023-03-15 19:23:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // // './pages/**/*.{html,js}',
+    // './public/{index.html, styles.css}'
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontSize: {
+        mammoth: '8rem',
+    }
+    },
+    fontFamily: {
+      body: ['Nunito']
+    }
+  },
+  plugins: [],
+}
